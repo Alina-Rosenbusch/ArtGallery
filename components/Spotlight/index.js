fb6e6ceae1db85b09c00bcd426353c9a58bb2e7d
Alina Rosenbusch <alina.rosenbusch@gmx.de>
2023-07-07 15:36:27 +0200

    add Art Piece Detail styling

--- components/Spotlight/index.js
+++ components/Spotlight/index.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 export default function Spotlight({ image, artist }) {
   return (
     <div className="spotligth">
-      <Image src={image} width={200} height={300} alt="Spotlight" />
+      <Image src={image} width={300} height={450} alt="Spotlight" />
       <p>{artist}</p>
     </div>
   );
